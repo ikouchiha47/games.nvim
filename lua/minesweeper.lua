ed73ff4fa613ff51e4daa6b73c7c3b37c70a37b3
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-10-10 19:02:21 +0530

    feat: rendering and moving cusors

--- lua/minesweeper.lua
+++ lua/minesweeper.lua
@@ -16,6 +16,9 @@ end
 function MineSweeper:init(size)
 	self.grid = {}
 	self.size = size
+	self.cursor = { x = 1, y = 1 }
+	self.game_over = false
+	self.game_start = false
 
 	for y = 1, size do
 		self.grid[y] = {}
@@ -59,28 +62,47 @@ local function vim_render(opts)
 end
 
 function MineSweeper:display()
+	if self.game_over then
+		return
+	end
+
+	if not self.game_start then
+		return
+	end
+
 	local display = ""
 
-	for y = 1, MineSweeper.height do
-		for x = 1, MineSweeper.width do
-			local cell = MineSweeper.grid[y][x]
+	for y = 1, self.size do
+		for x = 1, self.size do
+			local cell = self.grid[y][x]
+
 			if cell.flagged then
 				display = display .. MineSweeper.icons.flag .. " "
 			elseif not cell.revealed then
 				display = display .. MineSweeper.icons.hidden .. " "
 			elseif cell.bomb then
+				self.game_over = true
 				display = display .. MineSweeper.icons.bomb .. " "
 			else
 				display = display .. MineSweeper.icons.cleared .. " "
 			end
 		end
+
 		display = display .. "\n"
 	end
 
 	vim_render(vim.split(display, "\n"))
+
+	if self.game_over then
+		vim_render({ "", "Woohoo!! You blew up the house!" })
+		return
+	end
 end
 
-function MineSweeper:reveal_cell(x, y)
+function MineSweeper:reveal_cell()
+	local y = self.cursor.y
+	local x = self.cursor.x
+
 	local chosen = self.grid[y][x]
 
 	if chosen.revealed or chosen.flagged then
@@ -90,7 +112,10 @@ function MineSweeper:reveal_cell(x, y)
 	chosen.revealed = true
 end
 
-function MineSweeper:flag_cell(x, y)
+function MineSweeper:flag_cell()
+	local y = self.cursor.y
+	local x = self.cursor.x
+
 	local chosen = self.grid[y][x]
 
 	if chosen.revealed or chosen.flagged then
@@ -106,13 +131,89 @@ function MineSweeper:is_bomb(x, y)
 	return chosen.revealed and chosen.bomb
 end
 
+function MineSweeper:start()
+	self.game_start = true
+end
+
+function MineSweeper:has_started()
+	return self.game_start
+end
+
+function MineSweeper:move_cursor(dx, dy)
+	local new_x = self.cursor.x + dx
+	local new_y = self.cursor.y + dy
+
+	-- Make sure the cursor doesn't go out of bounds
+	if new_x >= 1 and new_x <= self.size then
+		self.cursor.x = new_x
+	end
+	if new_y >= 1 and new_y <= self.size then
+		self.cursor.y = new_y
+	end
+end
+
+local function remap_keys(buf, keychar, callback)
+	local cb = {
+		callback = callback,
+		noremap = true,
+		silent = true,
+	}
+	vim.api.nvim_buf_set_keymap(buf, "n", keychar, "", cb)
+end
+
 function MineSweeper.setup()
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_set_current_buf(buf)
+
 	local size = math.random(10, 20)
 	if size % 2 ~= 0 then
 		size = size + 1 -- Cox odd + odd = even, duh!
 	end
 
+	remap_keys(buf, "h", function()
+		MineSweeper:move_cursor(-1, 0)
+		MineSweeper:display()
+	end)
+
+	remap_keys(buf, "j", function()
+		MineSweeper:move_cursor(0, 1)
+	end)
+
+	remap_keys(buf, "k", function()
+		MineSweeper:move_cursor(0, -1)
+	end)
+
+	remap_keys(buf, "l", function()
+		MineSweeper:move_cursor(1, 0)
+	end)
+
+	remap_keys(buf, "<Space>", function()
+		MineSweeper:reveal_cell()
+		MineSweeper:display()
+	end)
+
+	remap_keys(buf, "<CR>", function()
+		if not MineSweeper:has_started() then
+			MineSweeper:start()
+		else
+			MineSweeper:flag_cell()
+		end
+
+		MineSweeper:display()
+	end)
+
 	MineSweeper:init(size)
+
+	vim_render({
+		"",
+		"Controls",
+		"Directions: [h|j|k|l]",
+		"Space: To flag a cell",
+		"Enter<CR> to reveal the cell",
+		"Enter to start",
+	})
 end
+
+return MineSweeper
 
 -- vim: ts=2 sts=2 sw=2 et
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,5 +1,7 @@
 local Hangman = require("hangman")
+local MineSweeper = require("minesweeper")
 
 vim.api.nvim_create_user_command("Hangman", Hangman.setup, {})
+vim.api.nvim_create_user_command("MineSweeper", MineSweeper.setup, {})
 
 -- vim: ts=2 sts=2 sw=2 et
